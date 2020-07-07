5d55a9b0f6b92fe0e095640a1f708dc5f93bfdd2
Eldrven <kszewc97@gmail.com>
2020-07-07 19:16:53 +0200

    Validate form and sending script

---
+++ script.php
@@ -0,0 +1,34 @@
+<?php
+$email_to = "#";
+$error=false;
+$name = $_POST["name"];
+$email_from = "test@acme.com";
+$email = $_POST["email"];
+$message = $_POST["message"];
+$email_subject = "Form submission from website";
+$headers = "From: " . $email_from . "\n";
+$headers .= "Reply-To: " . $email . "\n";
+
+$message = "Name: ". $name . "\r\nMessage: " . $message;
+
+if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+    echo $emailErr = 'Invalid email format  <br> <a href="/">Back to main page</a>';
+    $error=true;
+  }
+
+
+ini_set("sendmail_from", $email_from);
+if(!$error){
+
+    $sent = mail($email_to, $email_subject, $message, $headers, "-f" .$email_from);
+    if ($sent)
+{
+header("Location: /");
+} else {
+echo 'Oops! Something went wrong. Please try again later. <br> <a href="/">Back to main page</a>';
+}
+}
+
+
+
+?>
